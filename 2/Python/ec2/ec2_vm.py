b3b9d9f8e2a3e2765acee73769601810cbdd2504
Kishan Salgado <ksalgado@ksalgado-mn2.linkedin.biz>
2021-02-18 14:25:52 -0500

    Added ec2 folder

---
+++ 2/Python/ec2/ec2_vm.py
@@ -0,0 +1,13 @@
+import sys
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+# create a new EC2 instance
+instances = ec2.create_instances(
+     ImageId='ami-00843a337042b9b8b',
+     MinCount=1,
+     MaxCount=2,
+     InstanceType='t2.micro',
+     KeyName='ec2-keypair'
+ )
