4dc607a2e82041ec9370b220c3e45873574cf63b
ks <ksalgado@linkedin.com>
2021-03-20 11:35:44 -0400

    Added project1

---
+++ Week6-Serverless/Project1-BuildAzureFunctionApp/Readme.md
@@ -0,0 +1,5 @@
+# Project 1 - Creating FunctionApp in Azure
+
+1. Run the createFunctionApp.ps1 script
+2. Once the resources are deployed into Azure, create a new HTTP trigger function
+3. Update the existing code and test GET and POST methods.
---
+++ Week6-Serverless/Project1-BuildAzureFunctionApp/createFunctionApp.ps1
@@ -0,0 +1,31 @@
+function Create-FunctionApp {
+    param (
+        [parameter(Mandatory)]
+        [string]$RGName,
+
+        [parameter(Mandatory)]
+        [string]$name,
+
+        [parameter(Mandatory)]
+        [string]$storageAccountName
+    )
+
+    az group create 
+
+    az storage account create --name $storageAccountName `
+                              --resource-group $RGName
+
+    $plan = az functionapp plan create -g $RGName `
+                               -n $($name + 'plan') `
+                               --min-instances 1 `
+                               --max-burst 5 `
+                               --sku EP1
+    $plan
+    
+    az functionapp create -g $RGName `
+                          -n $name `
+                          -p $($name + 'plan') `
+                          --runtime powershell `
+                          -s $storageAccountName `
+                          --functions-version 2
+}
